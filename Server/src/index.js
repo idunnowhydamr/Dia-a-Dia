1257cb788b3f658996c6220bb6fdacf7bf3defb3
DiegoOrtiz27 <emanuelortizlopez@gmail.com>
2022-03-07 16:10:56 -0600

    Configuracion de entorno backend

---
+++ Server/src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan =  require('morgan');
+// Initializations
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 4000);
+
+// Middlewares
+app.use(morgan('dev'));
+
+// Global Variables
+
+// Routes
+app.use(require('./routes'))
+// Starting the server
+app.listen(app.get('port'), () =>{
+    console.log('Server on port:',app.get('port'))
+});
