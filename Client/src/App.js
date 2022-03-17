4850cf539aa9dc8a31af69b095a7c9e34344bfdf
diego munoz <diegomunoz@franciscomiranda.edu.co>
2022-03-16 22:41:48 -0500

    page sign in

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Registro from '../src/Pages/Registro';
+import "../src/App.css" 
+
 
 export default function App() {
   return (
