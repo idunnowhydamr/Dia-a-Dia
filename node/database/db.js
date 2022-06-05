38c9c55d8feaec621904fae1abd70f9a29855ec2
diego munoz <diegomunoz@franciscomiranda.edu.co>
2022-06-05 01:48:27 -0500

    Connection Data base from clever cloud

--- node/database/db.js
+++ node/database/db.js
@@ -1,7 +1,7 @@
 import {Sequelize} from 'sequelize';
 
-const db = new Sequelize('dia_a_dia', 'root', '', {
-    host:'localhost',
+const db = new Sequelize('btjoi9vkfs6ygcrb4lzp', 'urb4z8xwjd9c4fhv', 'EQ0sejRanawDdwwukTXe', {
+    host:'btjoi9vkfs6ygcrb4lzp-mysql.services.clever-cloud.com',
     dialect: 'mysql'
 })
 
